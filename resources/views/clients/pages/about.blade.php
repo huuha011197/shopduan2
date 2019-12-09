75767a764996be20044f96dd5318d91d0e580cc5
tnk102 <tnk301199.com>
2019-12-09 14:14:59 +0700

    edit page about and contact

--- resources/views/clients/pages/about.blade.php
+++ resources/views/clients/pages/about.blade.php
@@ -4,313 +4,45 @@ Giới thiệu
 @endsection
 @section('content')
 
-<div class="inner-header">
-	<div class="container">
-		<div class="pull-left">
-			<h6 class="inner-title">Giới thiệu</h6>
-		</div>
-		<div class="pull-right">
-			<div class="beta-breadcrumb font-large">
-				<a href="index.html">Home</a> / <span>Giới thiệu</span>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-</div>
-<div class="container">
-	<div class="content">
-		<h1 class="title">VỀ MiTA - HỆ THỐNG PHÂN PHỐI ĐỒNG HỒ CHÍNH HÃNG HÀNG ĐẦU VIỆT NAM</h1>
-		<div class="avatar-detail">
-			<img src="https://xwatch.vn/images/news/2019/01/01/original/banner-bao-hanh-trang-chu.jpg"
-				alt="Về Mita - Hệ Thống Phân Phối Đồng Hồ Chính Hãng Hàng Đầu Việt Nam">
-		</div>
-		<div class="summary">Không chỉ là bán đồng hồ, Mita còn mang cả đam mê, nhiệt huyết, sự chân thành gửi đến khách
-			hàng.</div>
-		<div class="description">
+<!-- Title Page -->
+<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url({{asset('client/images/anhd20.jpg')}});">
+	<h2 class="l-text2 t-center">
+		Giới Thiệu
+	</h2>
+</section>
 
-			<div id="toc">
-				<p></p>
-				<p>
-				</p>
-				<p id="toc-header">Nội dung bài viết </p>
-				<p></p>
-				<p>
-				</p>
-				<ul>
-					<p></p>
-					<p> </p>
-					<ol class="toc-2">
-						<li><a href="#2-danh-gia-cua-hang-Mita-ve-chat-luong-san-pham"
-								title="2/ Đánh Giá Cửa Hàng Mita Về Chất Lượng Sản Phẩm">2/ Đánh Giá Cửa Hàng Mita Về
-								Chất Lượng Sản Phẩm</a></li>
-						<li><a href="#3-danh-gia-che-do-bao-hanh---hau-mai-cua-Mita"
-								title="3/ Đánh Giá Chế Độ Bảo Hành - Hậu Mãi Của Mita">3/ Đánh Giá Chế Độ Bảo Hành - Hậu
-								Mãi Của Mita</a></li>
-					</ol>
-					<p></p>
-					<p> </p>
-				</ul>
-				<p></p>
-				<p> </p>
+<!-- content page -->
+<section class="bgwhite p-t-66 p-b-38">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 p-b-30">
+				<div class="hov-img-zoom">
+					<img src="{{asset('client/images/anhd1.jpg')}}" alt="IMG-ABOUT">
+				</div>
 			</div>
-			<p><em><span style="color:red"><strong>Chào mừng bạn đã đến với Mita, </strong></span></em> <span>Ngay khi
-					bạn đặt chân đến đây, chắc hẳn bạn đang muốn tìm hiểu thêm về Mita và quan tâm đến những mẫu đồng hồ
-					của chúng tôi.</span>
-				<span>Ngay lúc này, chúng tôi hiểu được những băn khoăn, trăn trở của bạn. Và tin rằng, phần lớn thắc
-					mắc của bạn sẽ được giải đáp ngay sau đây theo một cách chân thật nhất.</span></p>
-			<p></p>
-			<p></p>
-			<p><strong>1/ Đầu Tiên, Mita Là Ai?</strong></p>
-			<p></p>
-			<p></p>
-			<p><span>Theo cách nói chính thống nhất, Mita là một hệ thống phân phối đồng hồ chính hãng tại Việt Nam,
-					chuyên phân phối các thương hiệu đồng hồ Thụy Sĩ, Nhật Bản, Hoa Kỳ như: Seiko, Citizen, Orient,
-					Casio, OP (Olympianus, Olympia Star), Ogival, Elixa và Bulova.</span> <span>Ngoài ra, Mita còn có
-					một thương hiệu cao cấp hơn là </span><a href="https://Mitaluxury.vn/" rel="noopener noreferrer"
-					target="_blank"><span>Mita Luxury</span></a><span> - chuyên phân phối các thương hiệu đồng hồ Thụy
-					Sĩ cao cấp như: </span><span>Frederique Constant, Tissot, Emile Chouriet, Calvin Klein.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><img alt="" data-height="318" data-width="600" height="318"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-1.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<p><span>Thời điểm khởi đầu, Mita chỉ là một cửa hàng </span><span>nhỏ nằm trong con ngõ 44B Phất Lộc, Hoàn
-					Kiếm, Hà Nội - không tên tuổi, không ai biết đến! </span>
-			</p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center">
-				<img alt="showroom đồng hồ" data-height="396" data-width="600" height="396"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-2.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<p>
-				<span>Quy mô nhỏ, nhưng tâm huyết và khát vọng lớn! Mita được thành lập, đó là tâm huyết, là niềm đam mê
-					của những người thợ đồng hồ đem lòng “yêu” những cỗ máy thời gian, say mê với từng bánh răng, con
-					ốc. Đó là khát khao cháy bỏng: đẩy lùi nạn đồng hồ fake, trả lại sự trong sạch cho thị trường đồng
-					hồ Việt Nam, đem đến những giá trị đích thực cho người tiêu dùng.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center">
-				<img alt="đồng hồ phố huế" data-height="399" data-width="600" height="399"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-3.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<p><span>Ngược dòng thời gian từ những ngày đầu thành lập, thời điểm Mita lần đầu chập chững bước chân vào
-					thị trường đồng hồ Việt Nam. Sau quá trình tìm hiểu và thực nghiệm thị trường, chúng tôi nhận thấy
-					một thực trạng đáng buồn: tại Việt Nam, đồng hồ fake (giả) được bày bán công khai, tràn lan trên thị
-					trường bất chấp pháp luật; các đơn vị buôn đồng hồ giả sử dụng hàng trăm mánh khóe lừa đảo, trà trộn
-					hàng giả vào hàng thật và bán cho người tiêu dùng với giá cắt cổ.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center">
-				<iframe frameborder="0" height="315" src="https://www.youtube.com/embed/jCe1skozjZw"
-					width="560"></iframe></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><em><span>Vạch trần đồng hồ fake 200 triệu, vàng khối tinh vi</span></em></p>
-			<p></p>
-			<p></p>
-			<p><span>Người tiêu dùng Việt Nam - đáng nhẽ phải được hưởng những sản phẩm, dịch vụ tốt nhất, nhưng thay
-					vào đó chỉ là sự xem thường, lừa gạt của những đơn vị bán hàng không có TÂM. Chúng tôi thấy hổ thẹn
-					nếu chỉ biết đứng nhìn và phớt lờ, vì vậy, Mita ra đời với sứ mệnh mang “</span><strong>ĐỒNG HỒ THẬT
-					- GIÁ TRỊ THẬT</strong><span>” đến cho người tiêu dùng.</span></p>
-			<p></p>
-			<p></p>
-			<p><span>Trong suốt những năm qua, Mita đã tổ chức hàng loạt các chiến dịch vì cộng đồng theo đúng tôn chỉ
-					đặt ra như: Thẩm định đồng hồ thật giả MIỄN PHÍ cho hàng trăm nghìn khách hàng; trợ giá đổi đồng hồ
-					giả lấy thật; xây dựng website chuyên biệt giúp thẩm định đồng hồ qua ảnh </span><a
-					href="https://thamdinhdongho.vn/" rel="nofollow noopener noreferrer"
-					target="_blank"><span>https://thamdinhdongho.vn/</span></a><span>; phối hợp chặt chẽ với các
-				</span><span>Ban ngành chống hàng giả hàng nhái và hơn 30 cơ quan báo đài uy tín (VTV, Dân trí,
-					Vnexpress...) vạch mặt gian thương lừa đảo bán đồng hồ fake hàng nghìn đô, tiêu hủy hàng trăm chiếc
-					đồng hồ giả…</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><iframe frameborder="0" height="315"
-					src="https://www.youtube.com/embed/jw5EBwRpP4o" width="560"></iframe></p>
-			<p></p>
-			<p></p>
-			<p><span>Với những nỗ lực không ngừng nghỉ trong cuộc chiến chống đồng hồ fake, ngày 02/12/2015, Mita vinh
-					dự được trao Cúp vàng: Hàng thật – Thương hiệu chính hãng từ Viện Sở hữu trí tuệ Quốc tế.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><img alt="watch đồng hồ" data-height="305" data-width="600" height="305"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-4.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<p><span>Không chỉ dừng lại ở những hoạt động đẩy lùi đồng hồ fake, thẩm định đồng hồ thật giả cho người
-					tiêu dùng, Mita đã, đang và sẽ xây dựng nên một SÂN CHƠI đích thực dành cho những tín đồ đam mê cỗ
-					máy thời gian.</span></p>
-			<p></p>
-			<p></p>
-			<p><strong>* </strong><a href="https://www.facebook.com/Mitaluxury/" rel="noopener noreferrer"
-					target="_blank"><strong>FANPAGE Mita</strong></a><strong>:</strong><span> Cộng đồng đam mê đồng hồ
-					chính hãng lớn nhất tại Việt Nam với hơn 400.000 fans (01/12/2018). Đây là </span><span>nơi giao
-					lưu, chia sẻ và cập nhật những kiến thức chất nhất về đồng hồ, mang đến giá trị đích thực cho các
-					tín đồ đam mê đồng hồ chính hãng.</span></p>
-			<p></p>
-			<p></p>
-			<p><strong>* </strong><a href="https://www.youtube.com/channel/UC4P_yZzBLlxUKZyGygBHhkw"
-					rel="noopener noreferrer" target="_blank"><strong>XCHANNEL</strong></a><span>: Kênh review đồng hồ
-					đầu tiên tại Việt Nam với 84.218 người đăng ký, </span><span>49.538.368 lượt xem (01/12/2018).
-					XChannel là nơi cung cấp những video mới nhất về thị trường đồng hồ cùng với những “mổ xẻ”, đánh giá
-					khách quan về từng chiếc đồng hồ. </span></p>
-			<p></p>
-			<p></p>
-			<p><strong>* </strong><a href="https://www.Mita.vn/" rel="noopener noreferrer"
-					target="_blank"><strong>WEBSITE Mita.VN</strong></a><span>: Cung cấp những kiến thức từ A -&gt; Z về
-					đồng hồ: hướng dẫn sử dụng đồng hồ, cách phân biệt đồng hồ thật giả, tin tức thị trường đồng hồ.
-					Ngoài ra, đây là nơi “trưng bày” đầy đủ tất cả những mẫu đồng hồ hiện đang phân phối tại
-					Mita.</span> <span>Như vậy, qua những thông tin cơ bản trên, bạn có thể phần nào đánh giá về hoạt
-					động kinh doanh, tôn chỉ, sứ mệnh mà Mita theo đuổi. Phần tiếp theo sẽ giải quyết vấn đề: “Chất
-					lượng sản phẩm của Mita thế nào?”.</span></p>
-			<p></p>
-			<p></p>
-			<p><strong><em>&gt;&gt;&gt; Tìm hiểu thêm về </em></strong><a
-					href="https://www.Mita.vn/blog/triet-li-kinh-doanh-chu-tam-hang-dau-p2699.html"
-					rel="noopener noreferrer" target="_blank"><strong><em>triết lý kinh doanh của
-							Mita</em></strong></a><strong><em>!</em></strong></p>
-			<p></p>
-			<p></p>
-			<h2 id="2-danh-gia-cua-hang-Mita-ve-chat-luong-san-pham"><strong><span style="font-size:14pt">2/ Đánh Giá
-						Cửa Hàng Mita Về Chất Lượng Sản Phẩm</span> </strong></h2>
-			<p></p>
-			<p></p>
-			<p><span>Trước tiên, muốn biết sản phẩm của một cửa hàng có chất lượng hay không, hãy NGHE đánh giá từ chính
-					khách hàng của họ.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><iframe frameborder="0" height="315"
-					src="https://www.youtube.com/embed/CGiFuQ6tBEE" width="560"></iframe></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><em><span>Người nổi tiếng và khách hàng tin tưởng Mita như thế nào?</span></em>
-			</p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p><span>Với triết lý “</span><strong>Khách hàng là giá trị cốt lõi</strong><span>”, Mita luôn nỗ lực để đem
-					đến cho khách hàng những sản phẩm chất lượng, đảm bảo rõ ràng về nguồn gốc, xuất xứ, khiến khách
-					hàng hoàn toàn AN TÂM đặt trọn niềm tin vào Mita.</span></p>
-			<p></p>
-			<p></p>
-			<p><span>Đặc biệt, sau rất nhiều năm xây dựng và phát triển, từ một cửa hàng nhỏ tại Hà Nội, Mita đã lớn
-					mạnh và mở rộng mạng lưới trên toàn quốc với </span><strong><a
-						href="https://www.Mita.vn/blog/he-thong-x-watch-p3294.html" rel="noopener noreferrer"
-						target="_blank">12 showroom</a></strong><span> từ Bắc đến Nam. Chúng tôi hiểu rằng, quy mô càng
-					lớn thì càng phải chỉn chu trong chất lượng sản phẩm, dịch vụ sản phẩm và đặc biệt phải luôn giữ
-					vững giá trị cốt lõi: “</span><strong>Bán hàng bằng chữ TÂM” </strong><span>của mình.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><img alt="đồng hồ chính hãng cao cấp" data-height="247" data-width="600"
-					height="247"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-8.png"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<p><span>Chúng tôi cam đoan: Mọi sản phẩm đồng hồ đến tay khách hàng 100% là </span><strong>CHÍNH
-					HÃNG</strong><span>, nếu phát hiện đồng hồ fake, Mita sẵn sàng bồi thường gấp </span><strong>10
-					LẦN</strong><span> giá trị sản phẩm.</span> <span>Ngoài ra, toàn bộ các showroom thuộc hệ thống đồng
-					hồ chính hãng Mita đều được cấp giấy phép kinh doanh và có đầy đủ các giấy tờ pháp lý cần thiết.
-					Đồng thời, 100% sản phẩm đồng hồ của Mita đều được kiểm định chặt chẽ và nhập từ các nhà phân phối
-					chính hãng.</span></p>
-			<p></p>
-			<p></p>
-			<p><strong><em>&gt;&gt;&gt; Xem thêm: </em></strong><a
-					href="https://www.xwatch.vn/blog/xwatch-dai-ly-phan-phoi-dong-ho-chinh-hang-p3353.html"
-					rel="noopener noreferrer" target="_blank"><strong><em>Mita - đại lý phân phối đồng hồ chính
-							hãng</em></strong></a><em><span>.</span></em></p>
-			<p></p>
-			<p></p>
-			<p><span>Với những nỗ lực mang đến những sản phẩm đồng hồ chính hãng chất lượng cho người tiêu dùng, Mita đã
-					được </span><a
-					href="https://kinhdoanh.vnexpress.net/tin-tuc/doanh-nghiep/goi-y-dia-chi-ban-dong-ho-nam-danh-cho-doanh-nhan-3366498.html"
-					rel="noopener noreferrer" target="_blank"><span>báo VnExpress</span></a><span> đánh giá là một trong
-					những địa chỉ bán đồng hồ nam uy tín, tin cậy tại Việt Nam.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><img alt="đồng hồ chính hãng watch" data-height="210" data-width="600"
-					height="210"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-9.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<h2 id="3-danh-gia-che-do-bao-hanh---hau-mai-cua-Mita"><span style="font-size:14pt"><strong>3/ Đánh Giá Chế
-						Độ Bảo Hành - Hậu Mãi Của Mita</strong></span></h2>
-			<p></p>
-			<p></p>
-			<p><span>Một trong những yếu tố không thể thiếu khi đánh giá độ UY TÍN của một đơn vị phân phối đồng hồ
-					chính hãng chính là chế độ bảo hành, hậu mãi.</span> <span>Tại Mita, bạn hoàn toàn có thể hoàn toàn
-					an tâm với chế độ bảo hành, hậu mãi hàng đầu tại Việt Nam.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><img alt="cửa hàng bán đồng hồ chính hãng" data-height="247" data-width="600"
-					height="247"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-10.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<ul>
-				<p></p>
-				<p> </p>
-				<li><span>1 ĐỔI 1 trong vòng 30 ngày đối với lỗi nhà sản xuất</span></li>
-				<p></p>
-				<p> </p>
-				<li><span>Bảo hiểm CẢ LỖI NGƯỜI DÙNG trong vòng 5 năm.</span></li>
-				<p></p>
-				<p></p>
-			</ul>
-			<p></p>
-			<p></p>
-			<p><span>Đặc biệt, với gói </span><strong>Bảo hiểm</strong><strong> CẢ LỖI NGƯỜI DÙNG trong 5
-					năm</strong><span> - Đầu tiên tại Việt Nam, bạn sẽ tiết kiệm tối đa mọi chi phí, không chỉ là bảo
-					dưỡng mà còn cả khi sửa chữa những lỗi người dùng trong quá trình sử dụng</span><span>. Theo đó, với
-					những lỗi phát sinh do người dùng gây ra như: đồng hồ đi mưa bị vào nước, con cái nghịch làm trầy
-					xước, vỡ mặt kính, ăn lẩu khiến đồng hồ bị nhiễm từ, hấp hơi… Mita sẵn sàng bảo hành cho bạn!</span>
-			</p>
-			<p></p>
-			<p></p>
-			<p><span>&gt;&gt;&gt; Tìm hiểu về các Gói bảo hiểm khi mua đồng hồ chính hãng tại Mita, click </span><a
-					href="https://www.xwatch.vn/bao-hiem-loi-nguoi-dung" rel="noopener noreferrer"
-					target="_blank"><strong>TẠI ĐÂY</strong></a><span>.</span></p>
-			<p></p>
-			<p></p>
-			<p style="text-align:center"><img alt="đồng hồ hiệu chính hãng" data-height="247" data-width="600"
-					height="247"
-					src="https://www.xwatch.vn/wp-content/uploads/2017/03/xwatch-he-thong-phan-phoi-dong-ho-chinh-hang-11.jpg"
-					width="600"></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p><span>Quy trình BẢO HIỂM ĐỒNG HỒ tại Mita được thực hiện bởi đội ngũ chuyên gia giàu kinh nghiệm, được
-					đào tạo bài bản tại Thụy Sĩ, Nhật Bản. Với trung tâm bảo hành hiện đại, trang thiết bị và máy móc
-					tân tiến, chiếc đồng hồ của bạn sẽ được chăm sóc tỉ mỉ, đúng quy trình - mang lại sự an tâm tối đa
-					cho khách hàng.</span></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p></p>
-			<p><span>Với những lý giải khách quan và đánh giá đa chiều như trên, chắc hẳn bạn đã phần nào có những nhận
-					định, đánh giá về triết lý thương hiệu cũng như giá trị cốt lõi mà Mita theo đuổi. Tuy nhiên, để
-					đánh giá chính xác nhất, hãy tự mình trải nghiệm sản phẩm, dịch vụ tại Mita, chắc chắn bạn sẽ không
-					phải thất vọng!</span> <span>Nếu bạn còn thắc mắc hay cần tư vấn thêm về sản phẩm hay dịch vụ, hãy
-				</span><strong>CHAT</strong><span> hoặc gọi </span><strong>HOTLINE</strong>
-				<strong>1900.0325</strong><span> cho chúng tôi, Mita luôn sẵn lòng tư vấn cho bạn!</span></p>
-			<p></p>
-			<p></p>
+
+			<div class="col-md-8 p-b-30">
+				<h3 class="m-text26 p-t-15 p-b-16">
+					Lịch sử và Giá trị Thương hiệu mang lại
+				</h3>
+
+				<p class="p-b-28">
+					Được thành lập tại Seoul – Hàn Quốc năm 2001. Với những gì đã đạt được, Julius đã trở thành một thương hiệu lớn mang tầm quốc tế năm 2015. 
+				</p>
+
+				<div class="bo13 p-l-29 m-l-9 p-b-10">
+					<p class="p-b-11">
+						Trải qua quá trình 17 năm phát triển, Julius được phái đẹp chào đón nhiệt liệt chào đón khi xuất hiện  từ những ngày đầu tiên như Hàn Quốc sau đó lan tỏa và đăng ký hiệp hội Marid quốc tế trên 30 nước trên thế giới như America, Canada, Mexico, Iran, Vietnam, India, Philippines, Thai Lan, Trung Quốc. etc. 
+						Năm 2012, Julius được vinh danh với giải thưởng có thiết kế đẹp nhất năm 2012 tại hội chợ đồng hồ Hong Kong. 
+					</p>
+					<p>Julius được sản xuất khép kín từ khâu thiết kế tại Hàn Quốc, nhập khẩu máy của Nhật đến lắp ráp tại Trung Quốc. Tạo nên một mức giá phân khúc phổ thông phù hợp và cạnh tranh nhất. Xây dựng nên một “đế chế” vững mạnh trong lòng giới trẻ yêu thời trang. </p>
+					<p>Julius thương hiệu Hàn quốc, công nghệ Nhật Bản với máy toàn bộ khung máy đồng hồ được nhật nhập khẩu 100% từ Citizen. Thiết kế tại Korea bởi chuyên gia thời trang hàng đầu Hàn Quốc, tiêu chuẩn quốc tế, lắp ráp tại Trung Quốc, bảo hành quốc tế và chế độ hậu mãi tốt nhất.</p>
+					<span class="s-text7">
+						
+					</span>
+				</div>
+			</div>
 		</div>
 	</div>
-</div> <!-- .container -->
+</section>
 @endsection
